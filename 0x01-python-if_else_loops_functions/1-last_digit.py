608735ac9696171de7d3ef76f9fbb9b366f3954b
csoriano2832 <2832@holbertonschool.com>
2021-04-27 16:33:08 +0000

    Fix pep8 warnings

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 lastDigit = abs(number) % 10
 
 if number < 0:
-	lastDigit *= -1
+    lastDigit *= -1
 
-print ("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
+print("Last digit of {:d} is {:d}".format(number, lastDigit), end=" ")
 
 if lastDigit > 5:
-	print("and is greater than 5")
+    print("and is greater than 5")
 elif lastDigit == 0:
-	print("and is 0")
+    print("and is 0")
 else:
-	print("and is less than 6 and not 0")
+    print("and is less than 6 and not 0")

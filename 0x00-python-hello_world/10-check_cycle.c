8830d4b6b1790b3844116cdcb799c64518ecf83e
csoriano2832 <2832@holbertonschool.com>
2021-04-27 03:02:57 +0000

    Initialize variables to start of list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,14 +8,11 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow;
-	listint_t *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (list == NULL)
 		return (0);
-
-	slow = list->next;
-	fast = list->next->next;
 
 	while (fast && fast->next)
 	{

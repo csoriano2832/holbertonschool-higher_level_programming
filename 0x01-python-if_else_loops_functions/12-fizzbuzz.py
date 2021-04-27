644742cab641cf026f8ab9ed025e199e76983027
csoriano2832 <2832@holbertonschool.com>
2021-04-27 19:04:26 +0000

    Remove new line at the end

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 for number in range(1, 101):
-    if number != 100:
-        if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif number % 3 == 0:
-            print("Fizz", end=" ")
-        elif number % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print("{:d}".format(number), end=" ")
-        else:
-            print("Buzz")
+
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif number % 3 == 0:
+        print("Fizz", end=" ")
+    elif number % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print("{:d}".format(number), end=" ")

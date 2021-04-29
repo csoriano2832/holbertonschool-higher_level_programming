9a2f5a6f55a8d2bf65a8738e1777c9b06b1ed1e3
csoriano2832 <2832@holbertonschool.com>
2021-04-29 20:20:40 +0000

    Fix typos

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
     elif op == '*':
         print("{:d} {} {:d} = {:d}".format(num1, op, num2, mul(num1, num2)))
     elif op == '/':
-        if num2 != 0:
-            print("{:d} {} {:d} = {:d}".format(num1, op, num2, div(num1, num2)))
+        print("{:d} {} {:d} = {:d}".format(num1, op, num2, div(num1, num2)))
     else:
-        print("Unknown operator. Available operators: +, -, *, and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)

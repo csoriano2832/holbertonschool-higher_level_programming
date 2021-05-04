e6c6589d871668d19fb77a920c22e42148c92716
csoriano2832 <2832@holbertonschool.com>
2021-05-04 16:30:31 +0000

    Handle list = none edge case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
+    if my_list is not None:
+        my_list.reverse()
 
-    for x in range(len(my_list)):
-        print("{:d}".format(my_list[x]))
+        for x in range(len(my_list)):
+            print("{:d}".format(my_list[x]))

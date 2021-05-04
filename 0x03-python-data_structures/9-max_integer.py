8c12eeabfbcb50c0452a47aff097a856be9caa62
csoriano2832 <2832@holbertonschool.com>
2021-05-04 17:26:56 +0000

    Handle none edge case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     big_num = 0
 
-    if my_list is None:
-        return
+    if len(my_list) == 0:
+        return None
 
     for x in range(len(my_list) - 1):
         if my_list[x] > big_num:

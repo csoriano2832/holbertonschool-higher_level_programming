928d7dad56e0431d6ea9cbca490781f7820413b2
csoriano2832 <2832@holbertonschool.com>
2021-05-04 17:30:12 +0000

    Handle negative numbers

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    big_num = 0
-
     if len(my_list) == 0:
         return None
+
+    big_num = my_list[0]
 
     for x in range(len(my_list) - 1):
         if my_list[x] > big_num:

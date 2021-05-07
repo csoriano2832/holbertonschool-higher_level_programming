5ebd88bf21ec84bf4840b1e8ea825b4dbbb03982
csoriano2832 <2832@holbertonschool.com>
2021-05-07 03:14:06 +0000

    [Fix] pep8 error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,19 +5,9 @@ def roman_to_int(roman_string):
 
     result = 0
     index = 0
-    romanNumeralMap = (('M',  1000),
-                        ('CM', 900),
-                        ('D',  500),
-                        ('CD', 400),
-                        ('C',  100),
-                        ('XC', 90),
-                        ('L',  50),
-                        ('XL', 40),
-                        ('X',  10),
-                        ('IX', 9),
-                        ('V',  5),
-                        ('IV', 4),
-                        ('I',  1))
+    romanNumeralMap = (('M',  1000), ('CM', 900), ('D',  500), ('CD', 400),
+                       ('C',  100), ('XC', 90), ('L',  50), ('XL', 40),
+                       ('X',  10), ('IX', 9), ('V',  5), ('IV', 4), ('I',  1))
 
     for numeral, integer in romanNumeralMap:
         while roman_string[index:index+len(numeral)] == numeral:

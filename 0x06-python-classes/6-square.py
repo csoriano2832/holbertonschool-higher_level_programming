7c96137529727a12274297876b3d84d93d5ff4e6
csoriano2832 <2832@holbertonschool.com>
2021-05-18 20:23:32 +0000

    Fix position setter method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,11 +18,11 @@ class Square:
     @size.setter
     def size(self, value):
         ''' Setter method for size attribute '''
-        try:
-            self.__size = value
+        if type(value) is int:
             if value < 0:
                 raise ValueError("size must be >= 0")
-        except TypeError:
+            self.__size = value
+        else:
             raise TypeError("size must be an integer")
 
     @property
@@ -33,9 +33,11 @@ class Square:
     @position.setter
     def position(self, value):
         ''' Setter method for position attribute '''
-        try:
-            self.__position = value
-        except TypeError:
+        if type(value) is tuple and len(value) == 2:
+            if type(value[0]) is int and type(value[1]) is int:
+                if value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
+        else:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):

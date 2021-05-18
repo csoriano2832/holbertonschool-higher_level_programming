fbd0f5278f558336d5a01f601a5e4790769c626a
csoriano2832 <2832@holbertonschool.com>
2021-05-18 20:10:01 +0000

    Fix task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,8 @@ class Square:
     def __init__(self, size=0):
         '''This methods creates a new instance of a square'''
         try:
-            self.__size = size
             if size < 0:
                 raise ValueError("size must be >= 0")
+            self.__size = size
         except TypeError:
             raise TypeError("size must be an integer")

940a0102204558572e2179ac3eae6393f84ce371
csoriano2832 <2832@holbertonschool.com>
2021-05-18 20:16:26 +0000

    Handle float numbers

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,9 @@ class Square:
 
     def __init__(self, size=0):
         '''This methods creates a new instance of a square'''
-        try:
+        if type(size) is int:
             if size < 0:
                 raise ValueError("size must be >= 0")
             self.__size = size
-        except TypeError:
+        else:
             raise TypeError("size must be an integer")

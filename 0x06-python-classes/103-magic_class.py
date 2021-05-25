efe5f8ed258bdec5bf53e5cca9aabb0df0e22478
csoriano2832 <2832@holbertonschool.com>
2021-05-25 19:04:39 +0000

    Add documentation

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,14 +3,18 @@ import math
 
 
 class MagicClass():
+    """This class contains the methods area and circumference"""
 
     def __init__(self, radius=0):
+        """Constructor method"""
         if type(radius) is not int or if type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
+        """Returns the area"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Returns the circumference"""
         return 2 * math.pi * self.__radius

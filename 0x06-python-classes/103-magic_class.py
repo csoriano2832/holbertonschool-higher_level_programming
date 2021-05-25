cef0d7a496d22298df754bbef6107950b5bb9e3d
csoriano2832 <2832@holbertonschool.com>
2021-05-25 19:21:32 +0000

    Fix requirement errors

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""This module defines the class MagicClass"""
+'''This module defines the class MagicClass'''
+
 import math
 
 
 class MagicClass:
-    """This class contains the methods area and circumference"""
+    '''This class contains the methods area and circumference'''
 
     def __init__(self, radius=0):
-        """Constructor method"""
-        if type(radius) is not int or if type(radius) is not float:
+        '''Constructor method'''
+        if type(radius) is not int and if type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Returns the area"""
-        return self.__radius ** 2 * math.pi
+        '''Returns the area'''
+        return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Returns the circumference"""
-        return 2 * math.pi * self.__radius
+        '''Returns the circumference'''
+        return (2 * math.pi * self.__radius)

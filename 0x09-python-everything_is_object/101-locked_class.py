9a18cb19f78d021e65f451a3268063e98d40935d
csoriano2832 <2832@holbertonschool.com>
2021-05-26 00:28:31 +0000

    Add documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+'''This module contains a LockedClass class'''
+
+
 class LockedClass:
+    '''This class defines a class with only one attribute'''
     __slots__ = ['first_name']
 
     def __init__(self, value=""):
+        '''Constructor method'''
         self.first_name = value

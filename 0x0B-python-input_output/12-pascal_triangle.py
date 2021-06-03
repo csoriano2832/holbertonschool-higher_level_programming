89c299c9a8ecc88e26e83efc43b2320072ec3d51
csoriano2832 <2832@holbertonschool.com>
2021-06-03 21:45:30 +0000

    Add code to file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+'''
+This module contains 1 function(s):
+-----------------------------------
+pascal_triangle(n)
+
+'''
+
+
+def pascal_triangle(n):
+    ''' Returns a list of lists of integers representing
+    the Pascal's triangle of n '''
+
+    list_results = []
+
+    if n <= 0:
+        return list_results
+
+    for i in range(n):
+
+        row = [1]
+        if list_results:
+            l_row = list_results[-1]
+            row.extend([x + y for x, y in zip(l_row, l_row[1:])])
+            row.append(1)
+        list_results.append(row)
+
+    return list_results

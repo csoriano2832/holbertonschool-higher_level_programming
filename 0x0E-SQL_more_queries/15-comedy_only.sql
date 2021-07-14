ff5095f3caae75ec12f3e2380751e370c0e2185b
csoriano2832 <2832@holbertonschool.com>
2021-07-14 20:58:00 +0000

    edit to meet checker requirements

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,9 +1,9 @@
 -- Lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
-  FROM tv_shows INNER JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
- WHERE tv_show_genres.genre_id =
- 	   (SELECT id
-		  FROM tv_genres
-		 WHERE name = 'Comedy')
- ORDER BY tv_shows.title;
+	FROM tv_shows 
+		INNER JOIN tv_show_genres
+			ON tv_shows.id = tv_show_genres.show_id
+		INNER JOIN tv_genres
+			ON tv_genres.id = tv_show_genres.genres_id
+	WHERE tv_genres.name = 'Comedy'
+	ORDER BY tv_shows.title;

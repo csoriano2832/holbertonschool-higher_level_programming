18e898fdc609cd3317088865d2561804c0a5e7d9
csoriano2832 <2832@holbertonschool.com>
2021-07-14 20:59:59 +0000

    fix typo

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@ SELECT tv_shows.title
 		INNER JOIN tv_show_genres
 			ON tv_shows.id = tv_show_genres.show_id
 		INNER JOIN tv_genres
-			ON tv_genres.id = tv_show_genres.genres_id
+			ON tv_genres.id = tv_show_genres.genre_id
 	WHERE tv_genres.name = 'Comedy'
 	ORDER BY tv_shows.title;

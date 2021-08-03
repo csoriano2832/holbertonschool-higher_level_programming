ad6a0b1a7088467721cc5a4f6c66d84835787476
csoriano2832 <2832@holbertonschool.com>
2021-08-03 09:38:27 +0000

    fix sorting error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,6 +9,6 @@ if (argc < 4) {
   for (let index = 2; index < argc; index++) {
     arr.push(parseInt(argv[index], 10));
   }
-  arr.sort();
+  arr.sort(function (a, b) { return a - b; });
   console.log(arr[arr.length - 2]);
 }

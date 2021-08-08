8956417d75eb694e82e0f3040c15f39e0b6d083c
csoriano2832 <2832@holbertonschool.com>
2021-08-08 17:00:57 +0000

    Make changes to file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 ''' This module lists all states from a database '''
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    # Save argument list to variable
     argv = sys.argv
 
-    # Open database connection
+    # connect format (host, user, password, database)
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
-
-    # Create cursor object
     cursor = db.cursor()
-
-    # Execute SQL query
     cursor.execute("SELECT * FROM states ORDER BY states.id")
 
-    # Fetch and print all rows
     data = cursor.fetchall()
     for row in data:
         print(row)
 
-    # Disconnect from server
     db.close()

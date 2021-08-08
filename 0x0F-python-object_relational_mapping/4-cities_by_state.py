9f6ad1645333d153c8651fa89254185b5f45ff33
csoriano2832 <2832@holbertonschool.com>
2021-08-08 18:35:38 +0000

    add code to file

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' This module lists all cities from a database '''
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+
+    # connect format (host, user, password, database)
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM cities ORDER BY id")
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    db.close()

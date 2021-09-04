a3cc9bb2c60e907bf52ecf0a554c2a8710a9e3ea
csoriano2832 <2832@holbertonschool.com>
2021-09-04 16:09:46 +0000

    add file to repo

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Sends a POST request to a URL with a letter as parameter """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """ Only executes as main """
+
+    if len(sys.argv) == 2:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    try:
+        json = r.json()
+    except JSONDecodeError as e:
+        print("Not a valid JSON")
+    else:
+        if not json:
+            print("No result")
+        else:
+            print("[{}] {}".format(json.get('id'), json.get('name')))

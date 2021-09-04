357ab0f2e37e97a8408211d99f7ad7768520e5b7
csoriano2832 <2832@holbertonschool.com>
2021-09-04 19:10:21 +0000

    changing stuff

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
     commits = requests.get(url).json()
 

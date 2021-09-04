8b41b40b51f062b4a87b32fb55ea78c2734e6985
csoriano2832 <2832@holbertonschool.com>
2021-09-04 18:57:05 +0000

    add file to repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Gets the 10 newest commits in a repo """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    """ Only executes as main """
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
+
+    commits = requests.get(url).json()
+
+    for index in range(0, 10):
+        print(commits[index].get("sha"), end=": ")
+        print(commits[index].get("commit").get("author").get("name"))
